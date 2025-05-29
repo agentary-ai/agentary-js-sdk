40e561dd57084cd55b83328871dc0b766fc9b016
Declan Healey <declan.healey.1@gmail.com>
2025-05-29 08:45:55 -0700

    Enable generation of page prompts

--- src/core/AgentaryClient.js
+++ src/core/AgentaryClient.js
@@ -60,8 +60,8 @@ export class AgentaryClient extends EventEmitter {
         "bottom-right",
         {
           autoOpenOnLoad: true,
-          generateQuestions: false,
-          // maxQuestions: 5
+          generateQuestions: true,
+          maxQuestions: 5
         }
       );
     }
